385a959ba264bf974c12b3548ce8f81ef41c0a92
Yago Iglesias <pw17kto@eduvaud.ch>
2024-12-17 17:20:42 +0100

    feat:date + photos-OK

--- PhP/lamp/www/view/games.php
+++ PhP/lamp/www/view/games.php
@@ -7,13 +7,19 @@ ob_start();
 <h1>Matchs</h1>
 <table class="table table-bordered">
     <thead>
-        <tr><th>Numéro</th><th>Recevante</th><th>Visiteur</th><th>Score</th><th>Action</th></tr>
+        <tr><th>Date et heure</th><th>Numéro</th><th>Recevante</th><th>Visiteur</th><th>Score</th><th>Action</th></tr>
     </thead>
     <tbody>
     <?php
+    
+    
     foreach ($games as $game)
     {
-        echo "<tr><td>".$game->number."</td><td>".$game->receivingTeamName."</td><td>".$game->visitingTeamName."</td><td>".(($game->scoreReceiving+$game->scoreVisiting) > 0 ? $game->scoreReceiving."-".$game->scoreVisiting : "")."</td><td>";
+        // formatage de la date 
+        $date = new DateTime($game->moment);
+        //affichage de la date avec le bon format
+        echo "<tr><td>".$date->format('d M Y \à H:i')."</td>
+        <td>".$game->number."</td><td>".$game->receivingTeamName."</td><td>".$game->visitingTeamName."</td><td>".(($game->scoreReceiving+$game->scoreVisiting) > 0 ? $game->scoreReceiving."-".$game->scoreVisiting : "")."</td><td>";
         if ($game->isMarkable()) {
             echo "<a href='?action=mark&id=".$game->number."' class='btn btn-sm btn-primary m-1'>Marquer</a>";
         }
